e392a9bbcbe0b7522279eddf9cdf1daa560f31b5
PEDRO LANA <HENRIP@basfad.basf.net>
2022-12-03 18:02:14 -0300

    Fix file names and inserting placeholders to OracleDAO

--- NewLog/DAO/OracleDAO.cs
+++ NewLog/DAO/OracleDAO.cs
@@ -1,6 +1,16 @@
-﻿namespace NewLog.NewLog.Infra.Oracle.DAO
+﻿using System.Data;
+
+namespace NewLog.NewLog.Infra.Oracle.DAO
 {
     internal class OracleDAO
     {
+        public OracleDAO()
+        {
+        }
+
+        public DataTable Retrieve(int Code) 
+        {
+            return null;
+        }
     }
 }
